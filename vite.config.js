5df97dc902eebde72b9ccca0414134a079e9812b
kajalshukla521 <kajalshukl921@gmail.com>
2024-11-18 09:36:39 +0530

    fix the issues of icone,

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,12 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/redux-todolist/',  
   plugins: [react()],
   resolve: {
     alias: {
       '@': '/src',
-
-
-      
-        base: '/redux-todolist/',
-      
     },
   },
 });
